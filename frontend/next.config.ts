da022f4da343a30d6abd3845050c78579492f9bb
Aya Awad <145911633+ayaaawad@users.noreply.github.com>
2026-04-18 21:08:20 +0300

    Initial upload: frontend/next.config.ts

---
+++ frontend/next.config.ts
@@ -0,0 +1,18 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    devtoolSegmentExplorer: false,
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:4000/api/:path*',
+      },
+    ];
+  },
+};
+
+export default nextConfig;
